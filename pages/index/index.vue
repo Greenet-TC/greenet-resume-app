fa4194d364fe9468fa58288a09130c720f07a964
maxueming <maxueming@kuaishou.com>
2023-03-31 18:57:49 +0800

    feat: 新增gitignore

--- pages/index/index.vue
+++ pages/index/index.vue
@@ -1,52 +1,23 @@
 <template>
-	<view class="content">
-		<image class="logo" src="/static/logo.png"></image>
-		<view class="text-area">
-			<text class="title">1{{title}}</text>
-		</view>
-	</view>
+  <view class="content">
+    <tui-button width="372rpx" height="84rpx">页面主操作</tui-button>
+  </view>
 </template>
 
 <script>
-	export default {
-		data() {
-			return {
-				title: 'Hello'
-			}
-		},
-		onLoad() {
-
-		},
-		methods: {
-
-		}
-	}
+import tuiButton from "@/components/thorui/tui-button/tui-button.vue";
+export default {
+  components: {
+    tuiButton,
+  },
+  data() {
+    return {
+      title: "Hello",
+    };
+  },
+  onLoad() {},
+  methods: {},
+};
 </script>
 
-<style>
-	.content {
-		display: flex;
-		flex-direction: column;
-		align-items: center;
-		justify-content: center;
-	}
-
-	.logo {
-		height: 200rpx;
-		width: 200rpx;
-		margin-top: 200rpx;
-		margin-left: auto;
-		margin-right: auto;
-		margin-bottom: 50rpx;
-	}
-
-	.text-area {
-		display: flex;
-		justify-content: center;
-	}
-
-	.title {
-		font-size: 36rpx;
-		color: #8f8f94;
-	}
-</style>
+<style></style>
