7f8a1b70787ddffde6c413c5196876529be1dc7a
maxueming <maxueming@kuaishou.com>
2023-09-07 16:43:34 +0800

    feat: fix login

--- common/apis/weixin-login.ts
+++ common/apis/weixin-login.ts
@@ -2,25 +2,26 @@
  * @Author: maxueming maxueming@kuaishou.com
  * @Date: 2023-08-31 17:41:12
  * @LastEditors: maxueming maxueming@kuaishou.com
- * @LastEditTime: 2023-09-02 21:27:44
+ * @LastEditTime: 2023-09-07 15:25:11
  * @FilePath: /greenet-resume-app/common/apis/weixin-login.ts
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
 import http from "../../components/common/tui-request";
+import { setCookiesValue } from "../login";
 import { setToken } from "../utils";
 
 export const loginMiniGET = (code: string) => {
   return http
     .get(`/api/resume/web/loginMini?code=${code}`)
     .then((res) => {
-      console.log(1111,res.data);
+      console.log(1111, res.data, res.data.data.xxl_sso_sessionid);
       if (res.data.result === 1) {
         uni.showToast({
           title: "登录成功！",
           icon: "success",
           duration: 2000,
         });
-        setToken(res.data.data.xxl_sso_sessionid);
+        setCookiesValue(res.data.data.xxl_sso_sessionid);
         uni.removeStorageSync("preLogin");
       }
     })
--- store/index.ts
+++ store/index.ts
@@ -2,21 +2,22 @@
  * @Author: maxueming maxueming@kuaishou.com
  * @Date: 2023-09-04 20:23:46
  * @LastEditors: maxueming maxueming@kuaishou.com
- * @LastEditTime: 2023-09-04 21:09:08
+ * @LastEditTime: 2023-09-07 15:30:29
  * @FilePath: /greenet-resume-app/store/index.ts
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
 // 页面路径：store/index.js
-import { createStore } from 'vuex'
+import { createStore } from "vuex";
 import {
   WechatPayControllerQueryAccountGETResponse,
   userBaseInfoDataRespones,
-} from '../common/apis/model'
+} from "../common/apis/model";
 
 export interface IState {
-  userBaseInfo: userBaseInfoDataRespones | undefined
-  queryAccount: WechatPayControllerQueryAccountGETResponse | undefined
-  loginStatus: boolean | undefined
+  userBaseInfo: userBaseInfoDataRespones | undefined;
+  queryAccount: WechatPayControllerQueryAccountGETResponse | undefined;
+  loginStatus: boolean | undefined;
+  cookies: string;
 }
 const store = createStore<IState>({
   state: {
@@ -24,16 +25,19 @@ const store = createStore<IState>({
     userBaseInfo: undefined,
     queryAccount: undefined,
     loginStatus: undefined,
+    cookies: undefined,
   },
   mutations: {
     getUserInfo(state, info) {
-      // 变更状态
-      state.userBaseInfo = info
+      state.userBaseInfo = info;
     },
-    setLoginStatus(state,loginStatus) {
-        state.loginStatus = loginStatus
-    }
+    setLoginStatus(state, loginStatus) {
+      state.loginStatus = loginStatus;
+    },
+    setCookies(state, cookies) {
+      state.cookies = cookies;
+    },
   },
-})
+});
 
-export default store
+export default store;
