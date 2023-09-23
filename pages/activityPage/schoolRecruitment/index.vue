ff66da36fe59295a1c812abab47dc0a3e51236c4
maxueming <maxueming@kuaishou.com>
2023-09-23 17:30:03 +0800

    feat: fix build

--- pages/activityPage/schoolRecruitment/index.vue
+++ pages/activityPage/schoolRecruitment/index.vue
@@ -2,8 +2,15 @@
  * @Author: maxueming maxueming@kuaishou.com
  * @Date: 2023-09-12 11:26:53
  * @LastEditors: maxueming maxueming@kuaishou.com
- * @LastEditTime: 2023-09-12 11:56:10
+ * @LastEditTime: 2023-09-23 17:22:51
  * @FilePath: /greenet-resume-app/pages/activityPage/schoolRecruitment/index.vue
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
 -->
-<template></template>
+<template>
+  <view>
+    <img
+      src="https://mxm1923893223-ulteh-1302287111.tcloudbaseapp.com/activity/school-resume.png"
+      alt=""
+    />
+  </view>
+</template>
